dba6ec4cdc434081c91e6d4daba35625fbf2441d
paulokuda <paulyokuda@gmail.com>
2018-04-09 12:21:23 -0700

    Add buttons to trigger notifications

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="App">
@@ -11,8 +17,32 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          To get started, edit <code>src/App.js</code> and save to reload!
         </p>
+        <button
+          type="button"
+          onClick={() => alert('Default notification')}
+        >
+          Default notification
+        </button>
+        <button
+          type="button"
+          onClick={() => alert('Success notification')}
+        >
+          Success notification
+        </button>
+        <button
+          type="button"
+          onClick={() => alert('Error notification')}
+        >
+          Error notification
+        </button>
+        <button
+          type="button"
+          onClick={() => alert('Notification with timer')}
+        >
+           Notification with timer
+        </button>
       </div>
     );
   }
