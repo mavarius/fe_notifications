fb5fa7389ee5f5744d75bcab0e4d5887d00529a6
Zhichao Han <zhichao@fountain.com>
2018-05-16 16:19:18 -0700

    deleting content

--- src/App.js
+++ src/App.js
@@ -12,37 +12,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload!
-        </p>
-        <button
-          type="button"
-          onClick={() => alert('Default notification')}
-        >
-          Default notification
-        </button>
-        <button
-          type="button"
-          onClick={() => alert('Success notification')}
-        >
-          Success notification
-        </button>
-        <button
-          type="button"
-          onClick={() => alert('Error notification')}
-        >
-          Error notification
-        </button>
-        <button
-          type="button"
-          onClick={() => alert('Notification with timer')}
-        >
-           Notification with timer
-        </button>
+        Hello, World!
       </div>
     );
   }
